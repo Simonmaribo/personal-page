bf38840229da9655102b0a9fea978b285b1e7ab2
Simon maribo <64478800+Simonmaribo@users.noreply.github.com>
2022-04-07 20:59:58 +0200

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import { MantineProvider, NormalizeCSS, GlobalStyles } from '@mantine/core';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      theme={{
+        fontFamily: "Poppins, sans-serif",
+        colorScheme: 'light',
+        primaryColor: 'dodgerblue',
+        colors: {
+          'dodgerblue': ["#C3D6FF", "#99BAFF", "#73A0FF", "#5088FF", "#3173FF", "#145FFF", "#0050FF", "#0046EB", "#003FD4", "#0039BF", "#0033AB", "#002E9A", "#00298B" ],
+        }
+      }}
+    >
+    <NormalizeCSS />
+      <Component {...pageProps} />
+  </MantineProvider>
+  )
 }
 
 export default MyApp
